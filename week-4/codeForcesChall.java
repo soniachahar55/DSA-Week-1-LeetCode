5b1819cdcaf3bce14eea0afdbfc59fabaab27396
Sonia <164991323+soniachahar55@users.noreply.github.com>
2026-02-11 15:26:46 +0530

    Update codeForcesChall.java

--- week-4/codeForcesChall.java
+++ week-4/codeForcesChall.java
@@ -1,1 +1,35 @@
+// 1) Make it Divisible by 25
+// ques link: https://codeforces.com/contest/1593/problem/B
+
+import java.util.*;
+public class Main {
+    static int solve(String s, String target) {
+        int n = s.length();
+        int j = target.length() - 1; 
+        int deletions = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == target.charAt(j)) {
+                j--;
+                if (j < 0) return deletions; // found whole suffix
+            } else {
+                deletions++; // remove this digit
+            }
+        }
+        return (int)1e9; // impossible (won't happen as per problem guarantee)
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String n = sc.next();
+            int ans = Integer.MAX_VALUE;
+            ans = Math.min(ans, solve(n, "00"));
+            ans = Math.min(ans, solve(n, "25"));
+            ans = Math.min(ans, solve(n, "50"));
+            ans = Math.min(ans, solve(n, "75"));
+
+            System.out.println(ans);
+        }
+    }
+}
 
